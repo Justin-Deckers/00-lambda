c4eddeac21127065b1acc5c5b44f534d452b18ab
Justin <jdeckers@ucsc.edu>
2025-10-18 16:18:34 -0700

    INTEGRITY

---
+++ INTEGRITY.m
@@ -0,0 +1,7 @@
+I Utilized
+- Elsa Interactive Lambda Calculus Evaluator
+	-https://elsa.goto.ucsd.edu/index.html
+	-I used to easily evaluate my solutions
+- Elsa documentation
+	-https://github.com/ucsd-progsys/elsa
+	-I used to understand syntax and basic usage
